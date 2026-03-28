a36e4ee187142d9126d43b94992db174a2ad1e49
Davide Corio <davide.corio@dubhe.it>
2026-03-28 16:01:05 +0100

    [FIX] dub_cashflow: add missing data files to manifest (entry_match, recurring, settings, cron)

--- dub_cashflow/__manifest__.py
+++ dub_cashflow/__manifest__.py
@@ -14,17 +14,21 @@ Features:
 - Flexible field mapping with Python expressions
 - Filter records with domain expressions
     """,
-    "author": "Your Company",
-    "website": "https://www.yourcompany.com",
+    "author": "Dubhe Srls",
+    "website": "https://www.dubhe.it",
     "depends": [
         "account"
     ],
     "data": [
         "security/ir.model.access.csv",
         "views/cashflow_config_views.xml",
+        "views/cashflow_entry_match_views.xml",
         "views/cashflow_entry_views.xml",
         "views/cashflow_report_views.xml",
         "views/cashflow_item_views.xml",
+        "views/cashflow_recurring_views.xml",
+        "views/res_config_settings_views.xml",
+        "data/ir_cron.xml",
         "data/demo.xml",
     ],
     "pre_init_hook": "pre_init_hook",
